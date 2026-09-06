169a3bb068fc2cec8bd4449cc14e91f591fcc550
FelipeTinel <felipemacedotinel@gmail.com>
2026-09-06 12:10:03 -0300

    feat: add selection sort

---
+++ src/sorts/SelectionSort.cpp
@@ -0,0 +1,15 @@
+#include "SelectionSort.hpp"
+
+void SelectionSort::sort(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        int aux = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = aux;
+    }
+}
