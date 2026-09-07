7163d7009ca24bc9961833cdcf2900d7bf4af4ee
Mirella Costa <mirellabcost@gmail.com>
2026-09-07 15:16:46 -0300

    refactor: remove unused method

--- src/services/Statistics.cpp
+++ src/services/Statistics.cpp
@@ -1,4 +1,3 @@
-#include "../include/Statistics.hpp"
 #include "../../include/services/Statistics.hpp"
 
 Statistics::Statistics() {
@@ -9,7 +8,6 @@ void Statistics::reset() {
     this->nComparisons = 0;
     this->nSwaps = 0;
     this->startTime = std::chrono::high_resolution_clock::now();
-    this->endTime = std::chrono::high_resolution_clock::now();
     this->endTime = this->startTime;
 }
 
@@ -23,19 +21,13 @@ void Statistics::endTimer() {
 
 double Statistics::timeElapsed() const {
     std::chrono::duration<double, std::milli> duration = this->endTime - this->startTime;
-    duration /= 1000;
-    return duration;
     return duration.count();
 }
 
-int Statistics::getNComparisons(){ return this->nComparisons; }
-int Statistics::getNSwaps(){ return this->nSwaps; }
 unsigned long long Statistics::getNComparisons() const {
     return this->nComparisons;
 }
 
-void Statistics::incrementSwap(){ this->nSwaps++; }
-void Statistics::incrementComparison(){ this->nComparison++; }
 unsigned long long Statistics::getNSwaps() const {
     return this->nSwaps;
 }
